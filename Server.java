cea8f93f4197bf352c6c3093d44d9362bf8d3e60
Matt Owens <mgowens@utexas.edu>
2017-02-28 20:57:54 -0600

    get rid of ServerThread from inside Server

--- Server.java
+++ Server.java
@@ -50,27 +50,4 @@ public class Server {
 
     
   }
-
-  static class ServerThread implements Runnable {
-    Socket socket = null;
-
-    public ServerThread(Socket socket){
-      this.socket = socket;
-    }
-
-    public void run(){
-      try(
-        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(
-                new InputStreamReader(
-                    socket.getInputStream()));
-      ) {
-        //do client/server things here
-        
-
-      } catch (IOException e) {
-            e.printStackTrace();
-      }
-    }
-  }
 }
