a4502b3386b1bfe1d96e3e7e0af69e28284e08d9
Matt Owens <mgowens@utexas.edu>
2017-02-28 19:35:26 -0600

    TCP Server initial code

--- Server.java
+++ Server.java
@@ -1,3 +1,6 @@
+import java.net.*;
+import java.io.*;
+
 public class Server {
   public static void main (String[] args) {
     int tcpPort;
@@ -8,6 +11,17 @@ public class Server {
       System.out.println("\t(2) <udpPort>: the port number for UDP connection");
       System.out.println("\t(3) <file>: the file of inventory");
 
+      //BEGIN ESTABLISHING SERVER
+      try{
+        ServerSocket serverSocket = new ServerSocket(4444);
+        while(true){
+          Socket clientSocket = serverSocket.accept();
+
+
+        }
+
+      }
+
       System.exit(-1);
     }
     tcpPort = Integer.parseInt(args[0]);
@@ -16,6 +30,36 @@ public class Server {
 
     // parse the inventory file
 
+    //do server things
+    try (ServerSocket serverSocket = new ServerSocket(4444)){
+      while(true){
+        new ServerThread(serverSocket.accept()).start();
+      }
+    }
+
     // TODO: handle request from clients
+  }
+
+  public class ServerThread implements Runnable {
+    Socket socket = null;
+
+    public ServerThread(Socket socket){
+      super("ServerThread");
+      this.socket = socket;
+    }
+
+    public void run(){
+      try(
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                    socket.getInputStream()));
+      ) {
+        //do client/server things here
+
+      } catch (IOException e) {
+            e.printStackTrace();
+      }
+    }
   }
 }
